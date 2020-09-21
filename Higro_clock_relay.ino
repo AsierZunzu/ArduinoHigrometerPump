2620f15340726e72bd614a1c6effce813f9fa932
Asier Zunzunegui <asier.zunzu@gmail.com>
2020-09-21 18:25:15 +0200

    Additional led signaling + document functions

--- Higro_clock_relay.ino
+++ Higro_clock_relay.ino
@@ -6,9 +6,10 @@ RTC_DS3231 rtc;
 //Constants
 const int hygrometer = A0;  //Hygrometer sensor analog pin output at pin A0 of Arduino
 const int pump = 12;  //Hygrometer sensor analog pin output at pin A0 of Arduino
-const int hygrometerThreshold = 50;
-const int pumpAlertThreshold = 20; //s
-const int refreshingRate = 1000; //ms
+const int hygrometerThreshold = 50; //System will start pumping water when humidity is under this threshold, representing percentages.
+const int pumpAlertThreshold = 20; //The system will consider that if the time the pump has been active is higher thant this variable, a hardware error must have happened.
+const int refreshingRate = 1000; //ms, looping time
+const int sleepBlinkingRate = 1000; //ms
 const int alertBlinkingRate = 100; //ms
 
 //Variables
@@ -39,13 +40,20 @@ void setup() {
 }
 
 void loop() {
-  if (timeInRange()) {
-    hygrometerValue = getHygrometer();
-    pumpIsOn = managePump(hygrometerValue);
-    trackSecondsPumping(pumpIsOn);
-    checkSystemErrors();
+  if (!timeInRange()) {
+    Serial.print(" --> SLEEPING");
+    playSleepBlinkingSecence();
+    return;
   }
-
+  checkSystemErrors();
+  hygrometerValue = getHygrometer();
+  pumpIsOn = managePump(hygrometerValue);
+  if (pumpIsOn) {
+    Serial.print(" --> PUMP ON");
+    trackSecondsPumping();
+  } else {
+    resetSecondsPumping();
+  }
   Serial.println();
   delay(refreshingRate);
 }
@@ -57,20 +65,29 @@ bool managePump(int hygrometerValue)
     return false;
   } else {
     digitalWrite(pump, LOW);
-    Serial.print(" --> PUMP ON");
     return true;
   }
 }
 
-void trackSecondsPumping(bool pumpIsOn)
+/**
+   Adds a second to the pumping seconds counter, used to detect excessive pumping
+*/
+void trackSecondsPumping()
 {
-  if (pumpIsOn) {
-    secondsPumping++;
-  } else {
-    secondsPumping = 0;
-  }
+  secondsPumping++;
 }
 
+/**
+   Resets the pumping seconds counter
+*/
+void resetSecondsPumping()
+{
+  secondsPumping = 0;
+}
+
+/*
+   Checks for escesive pumping. If detected, runs an alert, and an infinite blinking loop.
+*/
 void checkSystemErrors()
 {
   if (secondsPumping > pumpAlertThreshold) {
@@ -79,6 +96,9 @@ void checkSystemErrors()
   }
 }
 
+/**
+   Runs an infinite blinking loop meaning the system found an error and it has stopped.
+*/
 void initilializeInfiniteBlinking()
 {
   while (true) {
@@ -89,6 +109,9 @@ void initilializeInfiniteBlinking()
   }
 }
 
+/**
+   checks time is in range, meaning it's not on sleepo hours. If it's not in range
+*/
 bool timeInRange()
 {
   DateTime now = rtc.now();
@@ -97,10 +120,26 @@ bool timeInRange()
   if ( now.hour() >= 10 && now.hour() <= 22) {
     return true;
   }
-  Serial.print(" --> SLEEP");
   return false;
 }
 
+/*
+   Runs 3 slow blinks, denoting that the system is sleeping
+*/
+void playSleepBlinkingSecence()
+{
+  for (int i = 0; i <= 3; ++i)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(sleepBlinkingRate);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(sleepBlinkingRate);
+  }
+}
+
+/*
+   Gets the hidrometer's value converted to percentages
+*/
 int getHygrometer()
 {
   hygrometerValueRaw = analogRead(hygrometer);     //Read analog value
