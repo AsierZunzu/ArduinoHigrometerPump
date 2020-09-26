853f2a52aa6571fce5fad7a753897e7acfa0337d
Asier Zunzunegui <asier.zunzu@gmail.com>
2020-09-26 16:42:48 +0200

    Remove inefficient hardware failure detection mechanism

--- hygro_clock_relay.ino
+++ hygro_clock_relay.ino
@@ -6,8 +6,7 @@ RTC_DS3231 rtc;
 //Constants
 const int hygrometer = A0;  //Hygrometer sensor analog pin output at pin A0 of Arduino
 const int pump = 12;  //Hygrometer sensor analog pin output at pin A0 of Arduino
-const int hygrometerThreshold = 50; //System will start pumping water when humidity is under this threshold, representing percentages.
-const int pumpAlertThreshold = 60; //The system will consider that if the time the pump has been active is higher thant this variable, a hardware error must have happened.
+const int hygrometerThreshold = 30; //System will start pumping water when humidity is under this threshold, representing percentages.
 const int refreshingRate = 1000; //ms, looping time
 const int sleepBlinkingRate = 1000; //ms
 const int alertBlinkingRate = 100; //ms
@@ -16,8 +15,6 @@ const int hygrometerConstraintLow = 300;
 
 //Variables
 int hygrometerValue , hygrometerValueRaw, hygrometerValueConstrain;
-bool pumpIsOn = true;
-int secondsPumping = 0;
 char timeBuffer[20] = "00-00-0000 00:00:00";
 
 void setup() {
@@ -47,78 +44,18 @@ void loop() {
     playSleepBlinkingSecence();
     return;
   }
-  checkSystemErrors();
-  hygrometerValue = getHygrometer();
-  pumpIsOn = managePump(hygrometerValue);
-  if (pumpIsOn) {
-    Serial.print(" --> PUMP ON");
-    trackSecondsPumping();
-  } else {
-    resetSecondsPumping();
-  }
+  hygrometerValue = getHygrometerValue();
+  managePump(hygrometerValue);
   Serial.println();
   delay(refreshingRate);
 }
 
-bool managePump(int hygrometerValue)
+void managePump(int hygrometerValue)
 {
   if (hygrometerValue > hygrometerThreshold) {
     stopPump();
-    return false;
   } else {
     startPump();
-    return true;
-  }
-}
-
-void stopPump()
-{
-  digitalWrite(pump, HIGH);
-}
-
-void startPump()
-{
-  digitalWrite(pump, LOW);
-}
-
-/**
-   Adds a second to the pumping seconds counter, used to detect excessive pumping
-*/
-void trackSecondsPumping()
-{
-  secondsPumping++;
-}
-
-/**
-   Resets the pumping seconds counter
-*/
-void resetSecondsPumping()
-{
-  secondsPumping = 0;
-}
-
-/*
-   Checks for escesive pumping. If detected, runs an alert, and an infinite blinking loop.
-*/
-void checkSystemErrors()
-{
-  if (secondsPumping > pumpAlertThreshold) {
-    Serial.println(F(" Excessive pumping detected, stopping all sistems now."));
-    stopPump();
-    initilializeInfiniteBlinking();
-  }
-}
-
-/**
-   Runs an infinite blinking loop meaning the system found an error and it has stopped.
-*/
-void initilializeInfiniteBlinking()
-{
-  while (true) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(alertBlinkingRate);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(alertBlinkingRate);
   }
 }
 
@@ -136,9 +73,9 @@ bool timeInRange()
   return false;
 }
 
-/*
-   Runs 3 slow blinks, denoting that the system is sleeping
-*/
+/**   
+ * Runs 3 slow blinks, denoting that the system is sleeping
+ */
 void playSleepBlinkingSecence()
 {
   for (int i = 0; i <= 3; ++i)
@@ -150,17 +87,18 @@ void playSleepBlinkingSecence()
   }
 }
 
-/*
-   Gets the hidrometer's value converted to percentages
-*/
-int getHygrometer()
+/**
+ * Gets the hidrometer's value converted to percentages. If hardware failure is found, it stops until it gets fixed.
+ */
+int getHygrometerValue()
 {
   hygrometerValueRaw = analogRead(hygrometer);
   Serial.print(" hygrometerValueRaw: " + String(hygrometerValueRaw));
-  if (hygrometerValueRaw >= hygrometerConstraintHigh || hygrometerValueRaw <= hygrometerConstraintLow) {
-    Serial.println(F(" Hygrometer failure detected, stopping all systems."));
+  while (hygrometerValueRaw >= hygrometerConstraintHigh || hygrometerValueRaw <= hygrometerConstraintLow) {
+    hygrometerValueRaw = analogRead(hygrometer);
+    Serial.println(F(" Hygrometer failure detected, pump stopped."));
     stopPump();
-    initilializeInfiniteBlinking();
+    errorBlink();
   }
   hygrometerValueConstrain = constrain(hygrometerValueRaw, hygrometerConstraintLow, hygrometerConstraintHigh); //Keep the ranges!
   
@@ -169,4 +107,25 @@ int getHygrometer()
   
   Serial.print(" Moisture: " + String(hygrometerValue) + "% ");
   return hygrometerValue;
+}
+
+void stopPump()
+{
+  digitalWrite(pump, HIGH);
+}
+
+void startPump()
+{
+  digitalWrite(pump, LOW);
+}
+
+/**
+ * Blinks once, meaning the system found an error.
+ */
+void errorBlink()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(alertBlinkingRate);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(alertBlinkingRate);
 }
